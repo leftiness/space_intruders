7fc65223d871468e0170ac7eec13f11f347773c4
Brandon Parmenter <leftiness@gmail.com>
2018-01-06 17:57:18 -0500

    Closes #21: Removed enemy spawn flag

--- src/system/enemy/spawn.lua
+++ src/system/enemy/spawn.lua
@@ -9,20 +9,11 @@ local TARGET = 15
 
 function System:initialize()
   self.filter = tiny.requireAll('isEnemy')
-  self.spawn = true
-end
-
-function System:onRemove(e)
-  self.spawn = #self.entities == 0
 end
 
 function System:onModify(dt)
-  if not self.spawn then
-    return
-  elseif #self.entities < TARGET then
+  if #self.entities < TARGET then
     self.world:add(Enemy(#self.entities))
-  else
-    self.spawn = false
   end
 end
 
